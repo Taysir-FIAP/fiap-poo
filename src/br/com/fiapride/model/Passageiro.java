26b6458a20522eb3ededd43822c9564fe4354c04
Taysir-FIAP <rm564884@fiap.com.br>
2026-05-05 20:04:02 -0300

    refactor: aplica encapsulamento na classe Passageiro e Objeto Pessoal

--- src/br/com/fiapride/model/Passageiro.java
+++ src/br/com/fiapride/model/Passageiro.java
@@ -2,14 +2,47 @@ package br.com.fiapride.model;
 
 // A Classe define a estrutura. Ela não é o passageiro real, é apenas o modelo.
 public class Passageiro {
-    public String nome;
-    public String cpf; // Novo Atributo
-    public double saldo;
+    // Agora ninguém fora desta classe consegue mexer nisso diretamente!
+    private String nome;
+    private String cpf;
+    private double saldo;
 
     public Passageiro(String nome, String cpf) {
+        this.setNome(nome);
+        this.setCpf(cpf);
+        this.setSaldo(0); // Todo mundo começa com zero!
+    }
+
+    // Método para PEGAR o saldo (Leitura)
+    public double getSaldo() {
+        return this.saldo; // Apenas devolve o valor, não altera nada.
+    }
+
+    // Método para DEFINIR o saldo (Escrita com Regra de Negócio!)
+    private void setSaldo(double valor) {
+        if (valor >= 0) {
+            this.saldo = valor;
+        } else {
+            System.out.println("Erro de Segurança: Tentativa de definir saldo negativo bloqueada!");
+        }
+    }
+
+    // Faça o mesmo para o nome (sem regras complexas por enquanto)
+    public String getNome() {
+        return this.nome;
+    }
+
+    private void setNome(String nome) {
         this.nome = nome;
-        this.cpf = cpf; // Novo Atributo
-        this.saldo = 0.00;
+    }
+
+    // Faça o mesmo para o cpf (sem regras complexas por enquanto)
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    private void setCpf(String cpf) {
+        this.cpf = cpf;
     }
 
     public void adicionarSaldo(double valor) {
